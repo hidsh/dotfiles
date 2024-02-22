31dd3afe506be710fd4ce065ef200a4524195a6b
hidsh <775213+hidsh@users.noreply.github.com>
2024-02-22 22:37:11 +0900

    mod nvim

--- d/nvim/init.lua
+++ d/nvim/init.lua
@@ -406,7 +406,7 @@ end
 vim.api.nvim_create_user_command('LiveGrepGitRoot', live_grep_git_root, {})
 
 -- See `:help telescope.builtin`
-vim.keymap.set('n', '<leader>?', require('telescope.builtin').oldfiles, { desc = '[?] Find recently opened files' })
+vim.keymap.set('n', '<A-r>', require('telescope.builtin').oldfiles, { desc = '[R] Find recently opened files' })
 vim.keymap.set('n', '<leader><space>', require('telescope.builtin').buffers, { desc = '[ ] Find existing buffers' })
 vim.keymap.set('n', '<leader>/', function()
   -- You can pass additional configuration to telescope to change theme, layout, etc.
--- d/nvim/my-add.lua
+++ d/nvim/my-add.lua
@@ -7,4 +7,20 @@ return {
       -- refer to the configuration section below
     },
   },
+  --[[
+  { "EdenEast/nightfox.nvim",
+    config = function()
+      require('nightfox').setup({
+        options = {
+          styles = {
+            -- comments = "italic",
+            -- keywords = "bold",
+            -- types = "italic,bold",
+          }
+        }
+      })
+    end
+  },
+  --]]
+
 }
