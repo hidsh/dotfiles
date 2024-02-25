fdb1a0b66f686556d0cdea83cfcf3c969d029bc1
hidsh <775213+hidsh@users.noreply.github.com>
2024-02-25 14:49:03 +0900

    mod nvim (kickstart.nvim)

--- d/nvim/my-add.lua
+++ d/nvim/my-add.lua
@@ -1,12 +1,88 @@
+-- keybindings
+-- misc.
+local o = vim.o
+
+o.expandtab = true			-- expand tab input with spaces characters
+o.smartindent = true		-- syntax aware indentations for newline inserts
+o.tabstop = 4				-- num of space characters per tab
+o.shiftwidth = 4			-- spaces per indentation level
+
+
+require("lazy").setup("plugins", {
+	change_detection = {
+		enabled = false,
+		notify = false,
+	},
+})
+
 return {
-  {
+-- plugins
+  { --------------------- theme
+    'rebelot/kanagawa.nvim',
+    config = function()
+        vim.cmd.colorscheme "kanagawa"
+    end,
+  },
+
+  --[[ disabled on goneovim
+  { --------------------- 
     "gbprod/yanky.nvim",
     opts = {
-      -- your configuration comes here
-      -- or leave it empty to use the default settings
-      -- refer to the configuration section below
     },
   },
+  --]]
+
+  { --------------------- 
+    'tzachar/highlight-undo.nvim',
+    opts = {
+      duration = 500,
+      undo = {
+        hlgroup = 'HighlightUndo',
+        mode = 'n',
+        lhs = 'u',
+        map = 'undo',
+        opts = {}
+      },
+      redo = {
+        hlgroup = 'HighlightUndo',
+        mode = 'n',
+        lhs = '<S-u>',
+        map = 'redo',
+        opts = {}
+      },
+      highlight_for_count = true,
+    },
+  },
+
+
+  { --------------------- 
+    'kqito/vim-easy-replace',
+    config = function()
+      vim.g.easy_replace_highlight_ctermbg = 'yellow'
+      vim.g.easy_replace_highlight_guibg = 'yellow'
+    end
+  },
+
+  --[[ template for nvim (Lua) plugins
+  { --------------------- 
+    '',
+    opts = {
+    }
+  },
+  
+  --]]
+
+  --[[ template for vim (Vim Script) plugins
+  { --------------------- 
+    '',
+    config = function()
+      vim.g.xxx = 
+      vim.g.yyy = 
+    end
+  },
+  
+  --]]
+
   --[[
   { "EdenEast/nightfox.nvim",
     config = function()
