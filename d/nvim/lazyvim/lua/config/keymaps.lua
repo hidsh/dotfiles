827550d9f2929064db106271137fcfcb079395a0
hidsh <775213+hidsh@users.noreply.github.com>
2024-02-29 04:16:29 +0900

    mod nvim, add LazyVim

---
+++ d/nvim/lazyvim/lua/config/keymaps.lua
@@ -0,0 +1,8 @@
+-- Keymaps are automatically loaded on the VeryLazy event
+-- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
+-- Add any additional keymaps here
+--
+--
+vim.keymap.set("n", "H", "<cmd>tabprevious<cr>", { desc = "Previous Tab" })
+vim.keymap.set("n", "L", "<cmd>tabnext<cr>", { desc = "Next Tab" })
+vim.keymap.set("n", "<C-x>k", "<cmd>tabclose<cr>", { desc = "Close Tab" })
---
+++ d/nvim/lazyvim/lua/config/util.lua
@@ -0,0 +1,7 @@
+local M = {}
+
+function M.toggle_option(option)
+  local value = not vim.api.nvim_get_option_value(option, {})
+  vim.opt[option] = value
+  vim.notify(option .. " set to " .. tostring(value))
+end
