6b21d5f57a177433c0916010500d926883342171
hidsh <775213+hidsh@users.noreply.github.com>
2024-03-05 05:59:34 +0900

    mod nvim, plugin config: bufferline, lualine, telescope

--- d/nvim/lazyvim/lua/plugins/plugins.lua
+++ d/nvim/lazyvim/lua/plugins/plugins.lua
@@ -1,15 +1,38 @@
--- since this is just an example spec, don't actually load anything here and return an empty spec
--- stylua: ignore
--- if true then return {} end
-
--- every spec file under the "plugins" directory will be loaded automatically by lazy.nvim
---
--- In your plugin files, you can:
--- * add extra plugins
--- * disable/enabled LazyVim plugins
--- * override the configuration of LazyVim plugins
 return {
-  { 'rebelot/kanagawa.nvim' },
-  { 'rafcamlet/nvim-luapad' },
-
+  { "rebelot/kanagawa.nvim" },
+  { "rafcamlet/nvim-luapad" },
+  { "nvim-telescope/telescope.nvim",
+    opts = {
+      defaults = {
+        mappings = {
+          i = {
+            ["<C-j>"] = "move_selection_next",
+            ["<C-k>"] = "move_selection_previous",
+          },
+        },
+      },
+    },
+  },
+  { "akinsho/bufferline.nvim",
+    opts = function(_, opts)
+      opts.options.show_buffer_close_icons = false
+      opts.options.show_close_icon = false
+    end,
+  },
+  { "nvim-lualine/lualine.nvim",
+    event = "VeryLazy",
+    dependencies = { "nvim-tree/nvim-web-devicons" },
+    opts = {
+      options = {
+        component_separators = "",
+        section_separators = "",
+      },
+      sections = {
+        lualine_b = {},
+        lualine_x = { "filetype" },
+        lualine_y = { "branch", "diff" },
+        lualine_z = { "location" },
+      },
+    },
+  },
 }
