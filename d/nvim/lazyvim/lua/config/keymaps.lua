b62cb8e38cd6712cb4949981310615b304428f0d
hidsh <775213+hidsh@users.noreply.github.com>
2024-03-06 09:44:36 +0900

    mod nvim, keymap

--- d/nvim/lazyvim/lua/config/keymaps.lua
+++ d/nvim/lazyvim/lua/config/keymaps.lua
@@ -3,7 +3,23 @@
 -- Add any additional keymaps here
 --
 -- Note: This file will be loaded after `plugins.lua`
-vim.keymap.set("n", ";", ":", { desc = "; acts as :" })
-vim.keymap.set("n", ":", ";", { desc = ": acts as ;" })
-
 vim.keymap.set("n", "<C-x>k", "<cmd>bd<cr>", { desc = "Close Tab" })
+vim.keymap.set({ "n", "v" }, ";", ":", { desc = "; acts as :" })
+vim.keymap.set({ "n", "v" }, ":", ";", { desc = ": acts as ;" })
+vim.keymap.set({ "n", "v" }, "3", "#", { desc = "3 acts as #" })
+vim.keymap.set({ "n", "v" }, "4", "$", { desc = "4 acts as $" })
+vim.keymap.set({ "n", "v" }, "8", "*", { desc = "8 acts as *" })
+vim.keymap.set({ "n", "v" }, "a", "A", { desc = "a acts as A" })
+vim.keymap.set({ "n", "v" }, "m", "<c-f>", { desc = "Page Down" })
+vim.keymap.set({ "n", "v" }, "M", "<c-b>", { desc = "Page Up" })
+vim.keymap.set("n", "U", "<cmd>redo<cr>", { desc = "redo" })
+
+function toggle_linum()
+  vim.opt.number = not vim.opt.number
+end
+vim.cmd("command! Linum lua toggle_linum()")
+
+function toggle_wrap()
+  vim.opt.wrap = not vim.opt.wrap
+end
+vim.cmd("command! TruncateLines lua toggle_wrap()")
